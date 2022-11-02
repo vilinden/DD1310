cb932590fdeafaaac33c07e1fb89c4ecd0a8f34c
VirreV <61963500+VirreV@users.noreply.github.com>
2022-11-02 13:17:44 +0100

    Almost there

--- Minesweeper/box.py
+++ Minesweeper/box.py
@@ -1,19 +1,34 @@
+import os
+from tkinter import *
+from tkinter import ttk
+
 class Box:
-    def __init__(self):
+    def __init__(self,x,y,window):
         self.__nearby = 0
+        self.__nearbyBoxes = []
         self.__mine = False
         self.__flagged = False
         self.__open = False
+        self.x = x
+        self.y = y
+        self.window = window
+        self.img = PhotoImage(file = f"blank.png")
+        self.__done = False
 
     def __str__(self):
         if self.__open:
             if self.__mine:
                 return "X"
-            else: return "0"
+            else: return str(self.__nearby)
         else: return ""
+
+
+    def setNearbyBoxes(self, boxes: list):
+        self.__nearbyBoxes = boxes
 
     def setMine(self):
         self.__mine = True
+        self.__nearby = "bomb"
     def getMine(self):
         return self.__mine
 
@@ -22,12 +37,48 @@ class Box:
     def getNearby(self):
         return self.__nearby
 
-    def setFlagged(self, flagged: bool):
-        self.__flagged = flagged
+    def setFlagged(self):
+        self.__flagged = not self.__flagged
+        if self.__flagged:
+            self.img = PhotoImage(file = f"flag.png")
+            self.label.config(image=self.img)
+        else:
+            self.img = PhotoImage(file = f"blank.png")
+            self.label.config(image=self.img)
+
     def getFlagged(self):
         return self.__flagged
 
+
+    def getBox(self, frame):
+        self.label = Label(frame, image=self.img)
+        self.label.grid(column = self.x, row = self.y)
+        self.label.bind("<Button-1>", lambda a:self.open())
+        self.label.bind("<Button-2>", lambda a:self.setFlagged())
+        self.label.bind("<Button-3>", lambda a:self.setFlagged())
+        return self.label
+
     def open(self):
-        self.__open = True
+        if not self.__open:
+            if not self.__mine:
+                self.__open = True
+                self.img = PhotoImage(file = f"{self.__nearby}.png")
+                self.label.config(image=self.img)
+                if self.__nearby == 0:
+                    for box in self.__nearbyBoxes:
+                        box.open()
+            else:
+                self.gameOver()
+
     def isOpen(self):
         return self.__open
+
+    def gameOver(self):
+        if not self.__done:
+            if not self.isOpen():
+                if self.__mine:
+                    self.img = PhotoImage(file = "bomb.png")
+                    self.label.config(image=self.img)
+            self.__done = True
+            for next in self.__nearbyBoxes:
+                next.gameOver()
--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -2,15 +2,17 @@ from box import Box
 import random
 from tkinter import *
 from tkinter import ttk
+from functools import partial
 
 # Huvudfunktion för att köra programmet
 def main():
-    size, mineCount = defInitials()
+    window = Tk()
+    size, mineCount, window = defInitials(window)
     board = []
     for y in range(size[1]):
         row = []
         for x in range(size[0]):
-            row.append(Box())
+            row.append(Box(x,y,window))
         board.append(row)
     
     mines = random.sample(range(len(board[0])*len(board)), mineCount)
@@ -19,47 +21,106 @@ def main():
         x = i%len(board[0])
         board[x][y].setMine()
 
-    draw(board)
+    for row in board:
+        for tile in row:
+            neighbors = []
+            coords = [
+                {"x": tile.x-1,  "y": tile.y-1},  #top right
+                {"x": tile.x-1,  "y": tile.y},    #top middle
+                {"x": tile.x-1,  "y": tile.y+1},  #top left
+                {"x": tile.x,    "y": tile.y-1},  #left
+                {"x": tile.x,    "y": tile.y+1},  #right
+                {"x": tile.x+1,  "y": tile.y-1},  #bottom right
+                {"x": tile.x+1,  "y": tile.y},    #bottom middle
+                {"x": tile.x+1,  "y": tile.y+1},  #bottom left
+            ]
+            for coord in coords:
+                try:
+                    if coord["x"] < 0 or coord["y"] < 0:
+                        raise
+                    neighbors.append(board[coord["y"]][coord["x"]])
+                except:
+                    pass
+            
+            neighborMines = 0
+            for n in neighbors:
+                if n.getMine():
+                    neighborMines += 1
+
+            tile.setNearby(neighborMines)
+            tile.setNearbyBoxes(neighbors)
+
+
+
+
+    draw(board,window)
 
 
 
 # Definierar initialvärden för brädesstorlek och antalet minor
-def defInitials():
+def defInitials(window):
+    x = 10
+    y = 10
+    mineCount = 6
     while True:
         try:
-            x = int(input("Enter board size (A x B)\nA: "))
-            y = int(input("B: "))
-            mineCount = int(input("Enter mine count: "))
-            if mineCount > x * y or x < 0 or y < 0 or mineCount < 0:
-                raise Exception
-            break
+            frame = ttk.Frame(window, padding=0)
+            frame.grid(pady=10)
+            Label(frame, text="Rows:").grid(row=1, padx=10, pady=5)
+            Label(frame, text="Columns:").grid(row=2, padx=10, pady=5)
+            Label(frame, text="Mines:").grid(row=3, padx=10, pady=5)
+
+            eRow = Entry(frame)
+            eCol = Entry(frame)
+            eMine = Entry(frame)
+
+            eRow.insert(10, str(x))
+            eCol.insert(10, str(y))
+            eMine.insert(10, str(mineCount))
+
+            eRow.grid(column=1, row=1, padx=10, pady=5, sticky="w")
+            eCol.grid(column=1, row=2, padx=10, pady=5, sticky="w")
+            eMine.grid(column=1, row=3, padx=10, pady=5, sticky="w")
+
+
+
+            def start():
+                x = int(eRow.get())
+                y = int(eCol.get())
+                mineCount = int(eMine.get())
+                if mineCount > x * y or x < 0 or y < 0 or mineCount < 0:
+                    raise Exception
+                window.destroy()
+
+            Button(frame, text="Start", command=start).grid(sticky="n", columnspan=2)
+
+            window.mainloop()
         except:
             print("---\nEnter a valid input\n---")
-    return [x,y], mineCount
+        finally:
+            window = Tk()
+            break
+    return [x,y], mineCount, window
+
 
 # Ritar brädet på skärmen
-def draw(board: list):
-    """
-    for y in range(len(board)):
-        for x in range(len(board[0])):
-            print(board[x][y], end="")
-        print()
-    """
-    window = Tk()
+def draw(board: list, window: Tk):
+
+    def restart():
+        window.destroy()
+        main()
+        window.quit()
+
     frm = ttk.Frame(window, padding=10)
     frm.grid()
     for y in range(len(board)):
         for x in range(len(board[0])):
-            box = ttk.Button(frm, text=board[x][y], command=lambda : board[x][y].open())
-            box.grid(column=x, row=y)
-            """frame = ttk.Frame(frm, text=board[x][y], width=50, height=50)
-            frame.grid(column=x, row=y)
-            frame.grid_propagate(False) #disables resizing of frame
-            frame.columnconfigure(0, weight=1) #enables button to fill frame
-            frame.rowconfigure(0,weight=1) #any positive number would do the trick
+            board[y][x].getBox(frm)
 
-            btn = ttk.Button(frame)
-            btn.grid(sticky="nswe")"""
+    frm2 = ttk.Frame(window, padding=10)
+    frm2.grid()
+    Button(frm2, text="Quit", command=lambda:window.quit()).grid(row=0,column=0, padx=10)
+    Button(frm2, text="Restart", command=restart).grid(row=0,column=1, padx=10)
     window.mainloop()
 
 main()
